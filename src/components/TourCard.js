61c6b6fece8194a3e550770a99c2e41401179001
Marcin Goszczynski <marcin.goszczynski@imgarena.com>
2025-01-21 16:17:41 +0100

    adding typography

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,4 +1,5 @@
-import { Paper, Grid } from "@mui/material";
+import { Paper, Grid, Typography, Box, Rating } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
 
 const TourCard = () => {
   return (
@@ -10,6 +11,37 @@ const TourCard = () => {
           }
           className={"img"}
         />
+        <Box paddingX={1}>
+          <Typography component={"h2"} variant={"subtitle1"}>
+            Immerse into the Falls
+          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant={"body2"} component={"p"} marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+            <Rating
+              name={"read-only"}
+              value={4.5}
+              precision={0.5}
+              size={"small"}
+              readOnly
+            />
+            <Typography variant={"body2"} component={"p"} marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant={"body3"} component={"p"} marginLeft={1.5}>
+              (655 reviews)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant={"h6"} component={"h3"} marginTop={0}>
+              From C $147
+            </Typography>
+          </Box>
+        </Box>
       </Paper>
     </Grid>
   );
