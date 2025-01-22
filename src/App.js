1269db5df6340f60a40e1c7b01b254375c9fc5da
Marcin Goszczynski <marcin.goszczynski@imgarena.com>
2025-01-22 11:23:30 +0100

    add app bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import TourCard from "./components/TourCard";
 import { Container, Grid } from "@mui/material";
+import SearchAppBar from "./components/AppBar";
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
         <Grid container spacing={5}>
           <TourCard />
         </Grid>
