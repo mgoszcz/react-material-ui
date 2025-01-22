cc748f17308d5fc1d361c9cf0eed7822063b7bcc
Marcin Goszczynski <marcin.goszczynski@imgarena.com>
2025-01-22 11:44:52 +0100

    creating cards with provided data

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -34,45 +34,40 @@ const theme = createTheme({
   },
 });
 
-const TourCard = () => {
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
       <ThemeProvider theme={theme}>
         <Paper elevation={3}>
-          <img
-            src={
-              "https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
-            }
-            className={"img"}
-          />
+          <img src={tour.image} alt={""} className={"img"} />
           <Box paddingX={1}>
             <Typography component={"h2"} variant={"subtitle1"}>
-              Immerse into the Falls
+              {tour.name}
             </Typography>
             <Box sx={{ display: "flex", alignItems: "center" }}>
               <AccessTime sx={{ width: 12.5 }} />
               <Typography variant={"body2"} component={"p"} marginLeft={0.5}>
-                5 hours
+                {tour.duration}
               </Typography>
             </Box>
             <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
               <Rating
                 name={"read-only"}
-                value={4.5}
+                value={tour.rating}
                 precision={0.5}
                 size={"small"}
                 readOnly
               />
               <Typography variant={"body2"} component={"p"} marginLeft={0.5}>
-                4.5
+                {tour.rating}
               </Typography>
               <Typography variant={"body3"} component={"p"} marginLeft={1.5}>
-                (655 reviews)
+                ({tour.nuberOfReviews} reviews)
               </Typography>
             </Box>
             <Box>
               <Typography variant={"h6"} component={"h3"} marginTop={0}>
-                From C $147
+                From C ${tour.price}
               </Typography>
             </Box>
           </Box>
