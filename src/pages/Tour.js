52c54c0be9409c4aa758308374277aa72b99b2e4
Marcin Goszczynski <marcin.goszczynski@imgarena.com>
2025-01-22 12:26:50 +0100

    other components

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,3 +1,63 @@
-const Tour = () => <div>Hello</div>;
+import {
+  BottomNavigation,
+  Box,
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/Accordion";
+import BasicModal from "../components/Modal";
+
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant={"h3"} component={"h1"} marginTop={3}>
+      Explore the World in Vegas
+    </Typography>
+    <Box marginTop={3} sx={{ display: "flex" }}>
+      <img
+        src={
+          "https://assets.simpleviewcms.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/lasvegas/strip_b86ddbea-3add-4995-b449-ac85d700b027.jpg"
+        }
+        alt=""
+        width={550}
+      />
+      <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant={"h6"} component={"h4"} marginTop={3}>
+        About this ticket
+      </Typography>
+      <Typography variant={"paragraph"} component={"p"} marginTop={3}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Typography>
+    </Box>
+    <Box marginBottom={10}>
+      <Typography
+        variant={"h6"}
+        component={"h4"}
+        marginTop={3}
+        marginBottom={2}
+      >
+        Frequently asked questions
+      </Typography>
+      <CustomizedAccordions />
+    </Box>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation>
+        <BasicModal />
+      </BottomNavigation>
+    </Paper>
+  </Container>
+);
 
 export default Tour;
